c130235d29333777a8eeee84541f3a608313303a
pchr8 <shamotskyi@gmail.com>
2019-07-30 07:45:22 +0200

    20190730_074522 dotfiles backup

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -23,3 +23,24 @@ mono = '10pt monospace'
 small_mono = '9pt monospace'
 
 c.hints.mode = 'number'
+
+config.bind('1', 'tab-focus 1')
+config.bind('2', 'tab-focus 2')
+config.bind('3', 'tab-focus 3')
+config.bind('4', 'tab-focus 4')
+config.bind('5', 'tab-focus 5')
+config.bind('6', 'tab-focus 6')
+config.bind('7', 'tab-focus 7')
+config.bind('8', 'tab-focus 8')
+config.bind('9', 'tab-focus 9')
+config.bind('0', 'tab-focus 10')
+config.bind('<Alt-1>', 'tab-focus 11')
+config.bind('<Alt-2>', 'tab-focus 12')
+config.bind('<Alt-3>', 'tab-focus 13')
+config.bind('<Alt-4>', 'tab-focus 14')
+config.bind('<Alt-5>', 'tab-focus 15')
+config.bind('<Alt-6>', 'tab-focus 16')
+config.bind('<Alt-7>', 'tab-focus 17')
+config.bind('<Alt-8>', 'tab-focus 18')
+config.bind('<Alt-9>', 'tab-focus 19')
+config.bind('<Alt-0>', 'tab-focus -1')
